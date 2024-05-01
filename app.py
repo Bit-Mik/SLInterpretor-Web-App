24d46e8e18fbe0a44b71ca2cb83eb133cc4102cb
Biswajit <iambitmik@gmail.com>
2024-05-01 16:37:27 +0530

    changes

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask,render_template,request
+# import cv2
+# from cvzone.HandTrackingModule import HandDetector
+# from cvzone.ClassificationModule import Classifier
+# import numpy as np
+# import math
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +13,6 @@ def landing_page():
 @app.route('/interpretor')
 def interpretor():
     return render_template ('interpretor.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
--- static/Js/app.js
+++ static/Js/app.js
@@ -1,37 +1,39 @@
 $(document).ready(function() {
     const video = document.getElementById('video');
     const canvas = document.getElementById('canvas');
-    const captureButton = document.getElementById('captureButton');
+    const ctx = canvas.getContext('2d');
 
     let mediaStream;
 
     // Start camera
     document.getElementById('startButton').addEventListener('click', async function() {
-        try {
-            mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
-            video.srcObject = mediaStream;
-        } catch (err) {
-            console.error('Error accessing camera:', err);
-        }
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function(stream) {
+                mediaStream = stream;
+                video.srcObject = stream;
+            })
+            .catch(function(err) {
+                console.error('Error accessing camera:', err);
+            });
+        
+            // Continuously capture frames and send them to backend
+            setInterval(function() {
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+                const imageData = canvas.toDataURL('image/jpeg');
+            
+                // Send captured frame to Flask backend
+                $.ajax({
+                    type: 'POST',
+                    url: '/process_frame',
+                    data: { frame_data: imageData },
+                    success: function(response) {
+                        console.log('Response from backend:', response);
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Error sending frame to backend:', error);
+                    }
+                });
+            }, 1); // Adjust the interval as needed;
     });
 
-    // Capture image
-    captureButton.addEventListener('click', function() {
-        const context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const imageData = canvas.toDataURL('image/png');
-
-        // Send captured image to Flask backend
-        $.ajax({
-            type: 'POST',
-            url: '/process_image',
-            data: { image_data: imageData },
-            success: function(response) {
-                console.log('Response from backend:', response);
-            },
-            error: function(xhr, status, error) {
-                console.error('Error sending image to backend:', error);
-            }
-        });
-    });
 });
